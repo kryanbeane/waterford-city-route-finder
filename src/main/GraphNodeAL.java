545bb7108537388f6d44d7cb571211b514f2f761
Peter English <englishpeter29@gmail.com>
2021-04-16 15:47:03 +0100

    Added x and y coords to GraphNodeAL

--- src/main/GraphNodeAL.java
+++ src/main/GraphNodeAL.java
@@ -1,19 +1,21 @@
 package main;
 import java.util.*;
 
-public class GraphNodeAL<T> {
-    public T data;
-    public List<GraphNodeAL<T>> adjList = new ArrayList<>();
+public class GraphNodeAL<T,K> {
+    public T xCoord;
+    public K yCoord;
+    public List<GraphNodeAL<T, K>> adjList = new ArrayList<>();
 
-    public GraphNodeAL(T data) {
-        this.data = data;
+    public GraphNodeAL(T xCoord, K yCoord) {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
     }
 
-    public void connectToNodeDirected(GraphNodeAL<T> destNode) {
+    public void connectToNodeDirected(GraphNodeAL<T, K> destNode) {
         adjList.add(destNode);
     }
 
-    public void connectToNodeUndirected(GraphNodeAL<T> destNode) {
+    public void connectToNodeUndirected(GraphNodeAL<T, K> destNode) {
         adjList.add(destNode);
         destNode.adjList.add(this);
     }
